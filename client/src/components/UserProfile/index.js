f79a36e2a1f8cdfdbb208fcf49fb5f3479c02539
“Janette <mujica.janette@gmail.com>
2022-11-25 13:29:56 -0500

    NavBar

--- client/src/components/UserProfile/index.js
+++ client/src/components/UserProfile/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import LogoutButton from '../LogoutButton';
 
 const UserProfile = () => {
 	const { user, isAuthenticated, isLoading } = useAuth0();
@@ -10,11 +11,15 @@ const UserProfile = () => {
 
 	return (
 		isAuthenticated && (
-			<div>
-				<img src={user.picture} alt={user.name} />
-				<h2>{user.name}</h2>
-				<p>{user.email}</p>
-			</div>
+			<>
+				<div>
+					<img src={user.picture} alt={user.name} />
+					<h2>{user.name}</h2>
+					<p>{user.email}</p>
+				</div>
+
+				<LogoutButton />
+			</>
 		)
 	);
 };
