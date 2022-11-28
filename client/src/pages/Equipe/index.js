3264d40adbfe674f560a6ffba5003bcdc55a0d3d
“Janette <mujica.janette@gmail.com>
2022-11-28 10:30:17 -0500

    Equipe sans les liens

--- client/src/pages/Equipe/index.js
+++ client/src/pages/Equipe/index.js
@@ -8,42 +8,68 @@ import Mathieu from '../../assets/mathieu.png';
 const Equipe = () => {
 	return (
 		<Wrapper>
-			<h1>Équipe</h1>
+			<TitrePage>ÉQUIPE</TitrePage>
+			<LesBios>
+				<section>
+					<img
+						src={Janette}
+						alt='Logo'
+						onMouseOver={(e) => (e.currentTarget.src = Codes)}
+						onMouseOut={(e) => (e.currentTarget.src = Janette)}
+					/>
 
-			<section>
-				<img
-					src={Janette}
-					alt='Logo'
-					onMouseOver={(e) => (e.currentTarget.src = Codes)}
-					onMouseOut={(e) => (e.currentTarget.src = Janette)}
-				/>
-				<p>Janette Mujica</p>
-				<p>CODEUSE ET DESIGNER</p>
-				<p>
-					Chez Ailleurs, je m’intéresse à mobiliser la connaissance vers
-					l'action.
-				</p>
-				<p>
-					Durant une douzaine d’année, j’ai optimisé la productivité de
-					dirigeants en tant qu’adjointe exécutive. J’entreprends une
-					réorientation de carrière qui vise à prioriser l’expérience
-					utilisateur. J’étudie l’innovation et l'entrepreneuriat à HEC Montréal
-					ainsi que le développement web à Concordia.
-				</p>
+					<Intro>
+						<Nom>Janette Mujica</Nom>
+						<Titre>CODEUSE ET DESIGNER</Titre>
+					</Intro>
+					<Statement>
+						Chez Ailleurs, je m’intéresse à mobiliser la connaissance vers
+						l'action.
+					</Statement>
+					<Bio>
+						Durant une douzaine d’année, j’ai optimisé la productivité de
+						dirigeants en tant qu’adjointe exécutive. J’entreprends une
+						réorientation de carrière visant à prioriser l’expérience
+						utilisateur en adoptant une posture empathique et holistique.
+						J’étudie l’innovation à HEC Montréal ainsi que le développement web
+						à Concordia.
+					</Bio>
+					<Bio>
+						Mon objectif est d’élaborer des services qui sont bons pour les
+						utilisateurs, les organisations et la société fondés sur des
+						processus de recherche et cocréation.
+					</Bio>
+				</section>
 
-				<p>CODEUSE ET DESIGNER.</p>
-			</section>
-
-			<section>
-				<img
-					src={Mathieu}
-					alt='Logo'
-					onMouseOver={(e) => (e.currentTarget.src = Famille)}
-					onMouseOut={(e) => (e.currentTarget.src = Mathieu)}
-				/>
-
-				<p></p>
-			</section>
+				<section>
+					<img
+						src={Mathieu}
+						alt='Logo'
+						onMouseOver={(e) => (e.currentTarget.src = Famille)}
+						onMouseOut={(e) => (e.currentTarget.src = Mathieu)}
+					/>
+					<Intro>
+						<Nom>Mathieu Bouchard</Nom>
+						<Titre>CRÉATEUR DE CONTENU</Titre>
+					</Intro>
+					<Statement>
+						Chez Ailleurs, je m’intéresse à la gouvernance et au financement de
+						l'innovation sociale.
+					</Statement>
+					<Bio>
+						Après des études en économie appliquée, j’ai travaillé huit ans
+						comme analyste en investissements et conseiller stratégique pour des
+						grandes caisses de retraite. J’ai ensuite quitté le milieu de
+						l’investissement pour compléter un doctorat en management à HEC
+						Montréal.
+					</Bio>
+					<Bio>
+						Mes travaux de recherche portent sur l’entraide, la pair-aidance,
+						l’apprentissage collectif et l’organisation par et pour les usagers
+						dans les systèmes professionnels.
+					</Bio>
+				</section>
+			</LesBios>
 		</Wrapper>
 	);
 };
@@ -55,7 +81,51 @@ const Wrapper = styled.div`
 	padding-left: 1em;
 	padding-right: 1em;
 	padding-bottom: 1em;
+`;
+
+const LesBios = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	gap: 2em;
 	img {
 		width: 150px;
 	}
+
+	section {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+		width: 343px;
+	}
 `;
+
+const TitrePage = styled.h1`
+	font-weight: 700;
+	margin-bottom: 1.5em;
+`;
+
+const Intro = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 0.5em;
+`;
+
+const Nom = styled.div`
+	font-size: 1.6em;
+	font-weight: 700;
+`;
+
+const Titre = styled.div`
+	color: var(--red);
+	font-size: 0.8em;
+	font-weight: 700;
+`;
+
+const Statement = styled.div`
+	font-size: 1.2em;
+	font-style: italic;
+	font-weight: 700;
+`;
+
+const Bio = styled.div``;
